5c46b375b3bd0a3fa714f4dea9bb6378aae6d62a
Thangabarath <tbarath20@gmail.com>
2021-05-23 21:07:43 +0530

    Adding HelloWorld.java

---
+++ Hands-on programs/Day-2/HelloWorld.java
@@ -0,0 +1,16 @@
+/* Example for simple java program using print statement */
+package com.onebill.corejava.basic;
+
+/**
+ * @author T. THANGABARATH
+ * @version
+ */
+public class HelloWorld {
+	public static void main(String[] args) {
+		int integer = 100;
+		long longvalue = 90876543;
+		System.out.println("Hello World");
+		System.out.println("Interger value = " + integer);
+		System.out.println("Long value = " + longvalue);
+	}
+}
