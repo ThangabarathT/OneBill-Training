615124a2499c855c4c58b0398cef0a276e5d265d
Thangabarath <tbarath20@gmail.com>
2021-05-23 21:18:05 +0530

    Adding IncrementDecrement.java

---
+++ Hands-on programs/Day-3/IncrementDecrement.java
@@ -0,0 +1,21 @@
+/* Example for Increment and Decrement Operators */
+package com.onebill.corejava.basic;
+
+/**
+ * @author T. THANGABARATH
+ * @version
+ */
+public class IncrementDecrement {
+
+	public static void main(String[] args) {
+		int first = 10;
+		int second = 5;
+		int third = 1;
+		int result;
+		result = first - ++third - ++second; // result = 10-2-6 = 2
+		if (true);
+		System.out.println(result); // print result value 2
+
+	}
+
+}
