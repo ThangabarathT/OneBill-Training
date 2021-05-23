41da222e7b90b4d6df7c923c06b71d961314159a
Thangabarath <tbarath20@gmail.com>
2021-05-23 21:19:02 +0530

    Adding StaticImport.java

---
+++ Hands-on programs/Day-3/StaticImport.java
@@ -0,0 +1,14 @@
+/* Example program for Static import method */
+package com.onebill.corejava.basic;
+
+import static java.lang.System.*; // Import package with static keyword
+
+/**
+ * @author T. THANGABARATH
+ * @version
+ */
+public class StaticImport {
+	public static void main(String[] args) {
+		out.println("Hello World!!!");
+	}
+}
