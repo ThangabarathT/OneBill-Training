a6d9f335a96e3cf37d9cf58a563e66f560c9b5fd
Thangabarath <tbarath20@gmail.com>
2021-05-25 08:24:00 +0530

    Adding InnerOuterClass.java

---
+++ Assignment programs/Day-5/InnerOuterClass.java
@@ -0,0 +1,28 @@
+/* Example program for Non-static nested Classes 
+ * 
+ * Inner class : One class contain inside another class
+ * 
+ * */
+package com.onebill.corejava.basic;
+
+/* This is the Outer class */
+class Outerclass {
+
+	/* This is the Inner class is inside the Outer class */
+	class Innerclass {
+		public void display() {
+			System.out.println("This is the Inner class");
+		}
+	}
+}
+
+public class InnerOuterClass {
+
+	public static void main(String[] args) {
+		/* Create a new Object for Inner class */
+		Outerclass.Innerclass inner = new Outerclass().new Innerclass();
+		inner.display();
+
+	}
+
+}
