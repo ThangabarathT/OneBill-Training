de63aa5981ddd81c09d9ddaaff5b79485dd7effa
Thangabarath <tbarath20@gmail.com>
2021-05-26 09:42:39 +0530

    Adding StringDemo.java

---
+++ Hands-on programs/Day-6/StringDemo.java
@@ -0,0 +1,15 @@
+package com.onebill.corejava.basics;
+
+public class StringDemo {
+
+	public static void main(String[] args) {
+		String s1 = "JAVA";
+		char[] ch = {'S','T','R','I','N','G'};
+		String s2 =new String(ch);
+		String s3 = new String("Onebill");
+		System.out.println(s1);
+		System.out.println(s2);
+		System.out.println(s3);
+	}
+
+}
