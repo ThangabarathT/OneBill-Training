29143b14326726c2c51c72fb5a196657fd3b7100
Thangabarath <tbarath20@gmail.com>
2021-05-29 10:15:01 +0530

    Adding SortById.java

---
+++ Hands-on programs/Day-9/SortById.java
@@ -0,0 +1,13 @@
+package com.onebill.corejava.collection;
+
+import java.util.Comparator;
+
+public class SortById implements Comparator<Employee> {
+
+	@Override
+	public int compare(Employee o1, Employee o2) {
+		return o1.empid - o2.empid;
+	}
+
+
+}
